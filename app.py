a43fd63c73750bfeee8a6f02e082fe5a15628fcc
soumilshah1995 <soushah@my.bridgeport.edu>
2019-12-15 19:12:17 -0500

    app.py added

---
+++ app.py
@@ -0,0 +1,29 @@
+try:
+    from Website import app
+    from flask import render_template
+
+    from flask import (Blueprint,
+                       render_template,
+                       redirect, url_for)
+
+    from flask import (Flask,
+                       request,
+                       redirect,
+                       session,
+                       send_file)
+
+    from io import BytesIO
+    from flask import abort, jsonify
+    import io
+
+except Exception as e:
+    print("Failed to load some Modules ")
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('Main.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
